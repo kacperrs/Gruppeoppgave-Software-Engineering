7a0a40d66764c5f4e77a5b252d73c1333e145a2a
Aleksander Lengard <aleksander.lengard@hiof.no>
2020-10-05 23:13:50 +0200

    Fjernet ubrukte tags

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -5,7 +5,6 @@
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam corrupti, deleniti rem temporibus quis aliquid enim deserunt illum, exercitationem ex reprehenderit, nihil culpa porro doloremque nobis dolorum iste laudantium consectetur!</p>
   
     <div class="notification is-grey-lighter">
-      <!-- <button class="delete"></button> -->
       Primar lorem ipsum dolor sit amet, consectetur
       adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Sit amet,
       consectetur adipiscing elit
@@ -17,9 +16,3 @@
 
   </div>
 </template>
-
-<style scoped>
-.home h1 {
-  text-align: center;
-}
-</style>
