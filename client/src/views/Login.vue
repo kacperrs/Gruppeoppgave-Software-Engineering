dd7fab25b5a9a2e1f5377636cce0664c2ba30d0b
Aleksander Lengard <aleksander.lengard@hiof.no>
2020-10-06 00:21:56 +0200

    Laget layout til login-from

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,5 +1,34 @@
 <template>
     <div class="login-form">
-        Hello login 🔥
+        <p class="title">Logg inn</p>
+        <div class="field">
+        <p class="control has-icons-left">
+            <input class="input is-medium" type="email" placeholder="Epost">
+            <span class="icon is-small is-left">
+            📧
+            </span>
+        </p>
+        </div>
+        <div class="field">
+        <p class="control has-icons-left">
+            <input class="input is-medium" type="password" placeholder="Passord">
+            <span class="icon is-small is-left">
+            🔒
+            </span>
+        </p>
+        </div>
+        <div class="field">
+        <p class="control">
+            <button class="button is-success is-medium is-fullwidth">
+            Login
+            </button>
+        </p>
+        </div>
     </div>
 </template>
+<style scoped>
+.login-form {
+    max-width: 50%;
+    margin: 0 auto;
+}
+</style>
