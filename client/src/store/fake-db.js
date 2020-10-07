47846cbafde7b4d00afd0bc5f640137face9d530
Aleksander Lengard <aleksander.lengard@hiof.no>
2020-10-08 00:16:38 +0200

    Laget en fake-db som kan brukes før api er oppe å går

---
+++ client/src/store/fake-db.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    username: "admin",
+    password: "password",
+    idAdmin: true
+  },
+  {
+    username: "bruker",
+    password: "welcome",
+    isAdmin: false
+  }
+];
+
+export default {
+  users
+};
