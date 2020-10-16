f04304207c434621f104144e74fbab023aa3b07a
Aleksander Lengard <aleksander.lengard@hiof.no>
2020-10-17 00:38:41 +0200

    Ny statisk database

---
+++ server/db/index.js
@@ -0,0 +1,8 @@
+// importer tabell filene
+import users from "./users.js";
+
+// eksporter dem her
+export { users };
+
+// Dette gjør slik at vi kan importere denne filen
+// å få tilgang til de tabellen vi trenger.
---
+++ server/db/users.js
@@ -0,0 +1,25 @@
+const users = [
+  {
+    firstname: "Kong",
+    lastname: "Olav",
+    email: "Olav@kongen.no",
+    password: "password",
+    isAdmin: true
+  },
+  {
+    firstname: "Ola",
+    lastname: "Normann",
+    email: "ola.normann@bring.no",
+    password: "welcome",
+    isAdmin: false
+  },
+  {
+    firstname: "Kari",
+    lastname: "Normann",
+    email: "kari.normann@posten.no",
+    password: "welcome",
+    isAdmin: false
+  }
+];
+
+export default users;
