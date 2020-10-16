0347059ffc9e7617a4893754ba6d79ba88fdb15d
Aleksander Lengard <aleksander.lengard@hiof.no>
2020-10-17 00:40:25 +0200

    Tester endepunkt til server :rocket:

---
+++ server/__tests__/server.test.js
@@ -0,0 +1,36 @@
+import supertest from "supertest";
+import { app } from "../server";
+import { users } from "../db/index.js";
+
+describe("GET /", () => {
+  it("Should respond with a message", async () => {
+    const response = await supertest(app)
+      .get("/")
+      .expect("Content-Type", /json/)
+      .expect(200);
+    expect(response.body.message).toEqual("Share-A-Spot Server - Up and 🏃");
+  });
+});
+
+describe("GET /<bad rout>", () => {
+  it("Should respond with a 404 status", async () => {
+    const badRout = "not_a_valid_rout";
+    const response = await supertest(app)
+      .get(`/${badRout}`)
+      .expect("Content-Type", /json/)
+      .expect(404);
+
+    expect(response.body.message).toEqual(`Not found - /${badRout}`);
+  });
+});
+
+describe("GET /users", () => {
+  it("Should respond with an array of users", async () => {
+    const response = await supertest(app)
+      .get(`/users`)
+      // .expect("Content-Type", /json/)
+      .expect(200);
+
+    expect(response.body).toEqual(users);
+  });
+});
