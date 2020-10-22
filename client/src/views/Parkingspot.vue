021d377dd458b70013a38fdb0824954feaa2595f
rikkehh <rikkehh@hiof.no>
2020-10-22 19:18:20 +0200

    Made a new update with parkingspot where we can show different by the selected.

--- client/src/views/Parkingspot.vue
+++ client/src/views/Parkingspot.vue
@@ -6,34 +6,44 @@
     <div class="class-use">
       Find parkingspot in Halden!
     </div>
-  <div class="is-justify-content-center data-list-input">
-    <select class="data-list-input" style="position: absolute; width: 200px " onchange="document.getElementById('displayValue').value=this.options[this.selectedIndex].text;
-    document.getElementById('idValue').value=this.options[this.selectedIndex].value;" v-model="places">
-      <option></option>
-      <option value="Tistedalen">Tistedalen</option>
-      <option value="Brødløs"> Brødløs </option>
-      <option value="Sentrum">Sentrum</option>
-      <option value="Busterud"> Busterud </option>
-      <option value="Berg">Berg</option>
-      <option value="Festningen"> Festingen</option>
-      <option value="Kornsjø"> Kornsjø </option>
-      <option value="Isebakke"> Isebakke </option>
-      <option value="Remmen"> Remmen </option>
-      <option value="Prestebakke"> Prestebakke</option>
+  <div class="is-justify-content-center data-list-input lister">
+    <select class="form-control" v-model="selected">
+      <option> Kornsjø</option>
+      <option> Brødløs</option>
+      <option>Tistedalen </option>
+      <option>Sentrum</option>
+      <option> Busterud </option>
+      <option> Berg </option>
+      <option>Festningen</option>
+      <option>Isebakken</option>
+      <option>Remmen</option>
+      <option> Prestebakken</option>
     </select>
 
   </div>
-
+  <div v-if="selected == 'Kornsjø'">
+    <div class="is-justify-content annonse1">
+    <h1> Parkeringsplass på Fjellåsveien 3</h1>
+    <p> Villa Kornsjø leier ut parkeringsplasser til 5 mennesker</p>
+    <a href="#">Trykk her for å komme inn på siden</a>
+    </div>
+  </div>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 
 export default {
-  name: "Parkingspot.vue"
-
+  name: "Parkingspot.vue",
+  el: '#lister',
+  data(){
+    return {
+    selected : 'Kornsjø'
+  }
+  }
 
 };
+
 
 </script>
 
