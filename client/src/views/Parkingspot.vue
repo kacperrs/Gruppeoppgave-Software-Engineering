7fe7071cfb45503f4427446ebaaae1f50cb547a3
rikkehh <rikkehh@hiof.no>
2020-10-26 21:42:09 +0100

    For loop over the option in the script. Got error!

--- client/src/views/Parkingspot.vue
+++ client/src/views/Parkingspot.vue
@@ -7,9 +7,11 @@
       Find parkingspot in Halden!
     </div>
   <div class="is-justify-content-center data-list-input lister">
-    <select class="form-control" v-model="selected">
-      <option> Kornsjø</option>
-      <option> Brødløs</option>
+    <select class="form-control" v-model="selected" >
+      <option v-for="option in $options" v-bind:key="option.id" v-bind:title="option.title"> {{option.title}}</option>
+      <!--
+      <option>Kornsjø</option>
+       <option> Brødløs</option>
       <option>Tistedalen </option>
       <option>Sentrum</option>
       <option> Busterud </option>
@@ -17,32 +19,45 @@
       <option>Festningen</option>
       <option>Isebakken</option>
       <option>Remmen</option>
-      <option> Prestebakken</option>
+      <option> Prestebakken</option> -->
     </select>
 
   </div>
-  <div v-if="selected == 'Kornsjø'">
-    <div class="is-justify-content annonse1">
-    <h1> Parkeringsplass på Fjellåsveien 3</h1>
-    <p> Villa Kornsjø leier ut parkeringsplasser til 5 mennesker</p>
-    <a href="#">Trykk her for å komme inn på siden</a>
+    <div>
+      <div v-if="selected == 'Kornsjø'">
+        Test
+      </div>
     </div>
   </div>
-  </div>
+
 </template>
 
 <script>
 
 export default {
-  name: "Parkingspot.vue",
-  el: '#lister',
+  name: "Parkingspot",
   data(){
+    options[
+            {id:1, title:'Kornsjø'},
+            {id:2, title:'Brødløs' },
+            {id:3, title:'Tistedalen'},
+            {id:4, title:'Sentrum'},
+            {id:5, title:'Busterud'},
+            {id:6, title:'Berg'},
+            {id:7, title:'Festningen'},
+            {id:8, title:'Isebakken'},
+            {id:9, title:'Remmen'},
+            {id:10, title:'Prestebakken'}
+        ]
     return {
-    selected : 'Kornsjø'
-  }
+      selected : '',
+
   }
 
+  },
+
 };
+
 
 
 </script>
