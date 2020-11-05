f0c8bda3715d5292d8d5bb0c1d2fdaada7cc3280
fabbebl <fabianbl@hiof.no>
2020-11-05 18:02:09 +0100

    Test for Git

---
+++ client/src/views/Payment.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Betaling</h2>
+        <div>
+            <p>Her kommer informasjon om hva som skal kjøpes og prisen. Eksempel:</p>
+            <p>[Produkt] - [Pris]</p>
+        </div>
+        <div>
+            <p>Betal med:</p>
+            <button>Vipps</button>
+            <button>Mastercard</button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
