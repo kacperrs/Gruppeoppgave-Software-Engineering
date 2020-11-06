937b44c3937be4c51ff3f1a8295e9140b368cf16
fabbebl <fabianbl@hiof.no>
2020-11-06 01:36:47 +0100

    Noen endringer i Payment

--- client/src/views/Payment.vue
+++ client/src/views/Payment.vue
@@ -5,18 +5,49 @@
             <p>Her kommer informasjon om hva som skal kjøpes og prisen. Eksempel:</p>
             <p>[Produkt] - [Pris]</p>
         </div>
-        <div>
+        <div id="PayBtn">
             <p>Betal med:</p>
-            <button>Vipps</button>
-            <button>Mastercard</button>
+            <button id="VipBtn">Vipps</button>
+            <button id="MasBtn">Mastercard</button>
+        </div>
+        <div id="replyDiv">
+        <p>Her kommer tilbakemelding om betalingen gikk igjennom eller ikke.</p>
+        <p>Denne div'en vil være skjult til betaling gjennomføres, og motsatt vil 'betal med' + knappene bli borte.</p>
         </div>
     </div>
 </template>
 
 <script>
+function runHiddenFunctions(){
+    makeHiddenVisible("replyDiv");
+    makeHiddenVisible("PayBtn");
+}
 
+function makeHiddenVisible(recievedDiv) {
+  if (recievedDiv.style.display === "none") {
+    recievedDiv.style.display = "block";
+  } else {
+    recievedDiv.style.display = "none";
+  }
+}
+document.getElementById("VipBtn").addEventListener("click", runHiddenFunctions());
+document.getElementById("MasBtn").addEventListener("click", runHiddenFunctions());
 </script>
 
 <style scoped>
+#replyDiv {
+    display: "none";
+}
 
+#VipBtn {
+    background-color: #f44336;
+    color: white;
+    border-radius: 12px;
+}
+
+#MasBtn {
+    background-color: #3366cc;
+    color: white;
+    border-radius: 12px;
+}
 </style>
---
+++ server/routes/payment.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+// husk: spots/
+router.get("/", );
+
+
+export default router;
