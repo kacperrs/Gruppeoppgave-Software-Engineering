439d5e9aef05972d07b28b786d45e40e7b07e93f
Aleksander Lengard <aleksander.lengard@hiof.no>
2020-11-14 23:23:48 +0100

    Lagt til v-calendar

--- client/src/main.js
+++ client/src/main.js
@@ -1,17 +1,20 @@
-import Vue from 'vue'
-import 'bulma/css/bulma.css';
+import Vue from "vue";
+import "bulma/css/bulma.css";
 
-import VueCompositionAPI from '@vue/composition-api'
+import VueCompositionAPI from "@vue/composition-api";
 Vue.use(VueCompositionAPI);
 
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import VCalendar from "v-calendar";
+Vue.use(VCalendar);
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount("#app");
--- client/src/views/parking/BookParkingspot.vue
+++ client/src/views/parking/BookParkingspot.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="bookparkingspot" class="container">
-    <p class="title">Bestille plass {{ spotid }}</p>
+    <p class="title">{{ spotData.address }}</p>
+    <p class="subtitle">{{ spotData.zipcode }}</p>
+    <div class="notification is-primary">
+      <v-date-picker
+        v-model="date"
+        :min-date="new Date()"
+        :disabled-dates="[{ weekdays: [1, 7] }, new Date(2020, 10, 17)]"
+        :v-model="date"
+        is-range
+        is-expanded
+      />
+    </div>
   </div>
 </template>
 
 <script>
-// import axios from "axios";
+import axios from "axios";
+
 export default {
   name: "BookParkingSpot",
   data() {
     return {
-      spotid: this.$route.params.id
+      spotid: this.$route.params.id,
+      spotData: {},
+      date: []
     };
+  },
+  created() {
+    this.getSpotData();
+  },
+  methods: {
+    getSpotData() {
+      axios
+        .get(`http://localhost:5000/spots/${this.$route.params.id}`)
+        .then((response) => {
+          this.spotData = response.data;
+        });
+    }
   }
 };
 </script>
