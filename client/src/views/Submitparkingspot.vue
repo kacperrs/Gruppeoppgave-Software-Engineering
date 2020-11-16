802dc837a81f2042ee1f585f658121c397e6e871
Aleksander Lengard <aleksander.lengard@hiof.no>
2020-11-16 23:25:21 +0100

    Fjernet antall plasser - og endert til tekst på stedsnavn

--- client/src/views/Submitparkingspot.vue
+++ client/src/views/Submitparkingspot.vue
@@ -1,12 +1,9 @@
 <template>
-
-
-	<div class="register-form">
+  <div class="register-form">
     <form action="#" @submit.prevent="submit" v-if="!registrationSuccess">
       <p class="title">Legg inn parkeringsplass</p>
 
-
-<div class="field is-horizontal">
+      <div class="field is-horizontal">
         <div class="field-label is-normal">
           <label class="label">Adresse</label>
         </div>
@@ -23,30 +20,28 @@
             </p>
           </div>
         </div>
-    </div>
- 
+      </div>
 
-
-        <div class="field is-horizontal">
+      <div class="field is-horizontal">
         <div class="field-label is-normal">
-          <label class="label">Postnummer</label>
+          <label class="label">Stedsnavn</label>
         </div>
         <div class="field-body">
           <div class="field">
             <p class="control">
               <input
                 class="input"
-                type="number"
-                placeholder="1890"
+                type="text"
+                placeholder="Oslo"
                 v-model="form.zipcode"
                 required
               />
             </p>
           </div>
         </div>
-    </div>
+      </div>
 
-        <div class="field is-horizontal">
+      <div class="field is-horizontal">
         <div class="field-label is-normal">
           <label class="label">Pris per time</label>
         </div>
@@ -63,9 +58,9 @@
             </p>
           </div>
         </div>
-    </div>
+      </div>
 
-        <div class="field is-horizontal">
+      <div class="field is-horizontal">
         <div class="field-label is-normal">
           <label class="label">Pris per dag</label>
         </div>
@@ -82,9 +77,9 @@
             </p>
           </div>
         </div>
-    </div>
+      </div>
 
-    <div class="field is-horizontal">
+      <!-- <div class="field is-horizontal">
         <div class="field-label is-normal">
           <label class="label">Antall parkeringsplasser</label>
         </div>
@@ -101,50 +96,36 @@
             </p>
           </div>
         </div>
-    </div>
+    </div> -->
 
-
-    <div class="field is-horizontal">
+      <div class="field is-horizontal">
         <div class="field-label is-normal">
           <label class="label"></label>
         </div>
         <div class="field-body">
           <div class="field">
             <p class="control">
-              <button  class="button is-success is-medium is-fullwidth mt-5">
+              <button class="button is-success is-medium is-fullwidth mt-5">
                 Legg inn parkeringsplass
               </button>
             </p>
           </div>
         </div>
       </div>
+    </form>
 
-</form>
-
-<div class="notification is-success" v-show="registrationSuccess">
+    <div class="notification is-success" v-show="registrationSuccess">
       <p class="title">👏 Takk!</p>
       <p>
         Parkeringsplassen er nå lagt inn!
       </p>
     </div>
-
-</div>
-
-
-
-
-
-
-
-
-
-
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-	
   data() {
     return {
       form: {
@@ -161,44 +142,34 @@ export default {
   },
 
   methods: {
-
     async submit() {
       this.form.ownerId = this.token;
       console.log(this.form);
-     await axios
-    
-       .post("http://localhost:5000/spots/",this.form, {
+      await axios
+
+        .post("http://localhost:5000/spots/", this.form, {
           headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json"
           }
         })
         .then((response) => {
           if (response.status === 201) {
             this.registrationSuccess = true;
             console.log("data har blitt sendt inn");
-          
           }
           if (response.status === 500) {
-           console.log("Det har oppstått en feil!");
+            console.log("Det har oppstått en feil!");
           }
         });
-       
     }
   },
-
-   
-
-
 
   computed: {
     token() {
       return this.$store.getters.token;
     }
-  },
-
-  
+  }
 };
-
 </script>
 
 <style scoped>
@@ -206,7 +177,4 @@ export default {
   max-width: 80%;
   margin: 0 auto;
 }
-
-
-
 </style>
